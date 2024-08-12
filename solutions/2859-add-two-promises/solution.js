ba4fe592447cbf89f0e85c8f6bb4b81c4e9445ab
Nguyen Minh Dat <datnm.ptit@gmail.com>
2024-08-12 16:18:34 +0000

    [LeetCode Sync] Runtime - 55 ms (90.03%), Memory - 49.4 MB (75.47%)

---
+++ solutions/2859-add-two-promises/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const result = await Promise.all([promise1, promise2])
+    return result.reduce((acc, curr) => {
+        return acc + curr
+    }, 0)
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
