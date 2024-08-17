8866e3f0d0ffec996cc94b248c62b8d19b821fe8
Nguyen Minh Dat <datnm.ptit@gmail.com>
2024-08-17 21:22:56 +0000

    [LeetCode Sync] Runtime - 764 ms (99.70%), Memory - 0B (100.00%)

---
+++ solutions/1773-percentage-of-users-attended-a-contest/solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+
+SELECT
+    contest_id,
+    ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage
+FROM
+    Register
+GROUP BY
+    contest_id
+ORDER BY
+    percentage DESC,
+    contest_id ASC
