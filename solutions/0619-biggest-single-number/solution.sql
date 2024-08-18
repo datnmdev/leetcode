96d2191779149654f044f338a215a67780d9b90e
Nguyen Minh Dat <datnm.ptit@gmail.com>
2024-08-18 06:53:25 +0000

    [LeetCode Sync] Runtime - 334 ms (93.54%), Memory - 0B (100.00%)

---
+++ solutions/0619-biggest-single-number/solution.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    MAX(num) as num
+FROM
+    (SELECT
+        num
+    FROM
+        MyNumbers
+    GROUP BY
+        num
+    HAVING
+        COUNT(num) = 1) AS T
